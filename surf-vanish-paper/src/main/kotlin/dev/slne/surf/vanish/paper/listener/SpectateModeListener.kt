cfe82cff80c0511b2fdbb5c8e23e1a180840fb4b
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-07-17 17:47:09 +0200

    feat: update glowing mechanism in SpectateModeListener to use glowingApi

--- surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/listener/SpectateModeListener.kt
+++ surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/listener/SpectateModeListener.kt
@@ -1,10 +1,8 @@
 package dev.slne.surf.vanish.paper.listener
 
-import com.github.retrooper.packetevents.PacketEvents
-import com.github.retrooper.packetevents.protocol.entity.data.EntityData
-import com.github.retrooper.packetevents.protocol.entity.data.EntityDataTypes
-import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityMetadata
+import dev.slne.surf.surfapi.bukkit.api.glow.glowingApi
 import dev.slne.surf.vanish.core.service.util.PluginMessageChannels
+import net.kyori.adventure.text.format.NamedTextColor
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.bukkit.plugin.messaging.PluginMessageListener
@@ -40,13 +38,11 @@ class SpectateModeListener : PluginMessageListener {
                 val player = Bukkit.getPlayer(uuid) ?: return
                 val targetPlayer = Bukkit.getPlayer(target) ?: return
 
-                PacketEvents.getAPI().playerManager.sendPacket(player,
-                    WrapperPlayServerEntityMetadata(
-                        targetPlayer.entityId,
-                        listOf(
-                            EntityData(0, EntityDataTypes.BYTE,  if (glow) 0x40.toByte() else 0x00.toByte()),
-                        )
-                    ))
+                if(glow) {
+                    glowingApi.makeGlowing(targetPlayer, player, NamedTextColor.RED)
+                } else {
+                    glowingApi.removeGlowing(targetPlayer, player)
+                }
             }
         }
     }
