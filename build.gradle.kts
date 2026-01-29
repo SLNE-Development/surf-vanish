c29b6c4ff990a7c3e1be7749c7ad79ee4245fb0f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-01-29 18:39:42 +0100

    chore: re-enable slneReleases repository for subprojects and bump version to 1.21.11-1.0.5-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import dev.slne.surf.surfapi.gradle.util.slneReleases
+
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -13,14 +15,14 @@ allprojects {
     group = "dev.slne.surf.vanish"
 }
 
-//subprojects {
-//    afterEvaluate {
-//        plugins.withType<PublishingPlugin> {
-//            configure<PublishingExtension> {
-//                repositories {
-//                    slneReleases()
-//                }
-//            }
-//        }
-//    }
-//}
+subprojects {
+    afterEvaluate {
+        plugins.withType<PublishingPlugin> {
+            configure<PublishingExtension> {
+                repositories {
+                    slneReleases()
+                }
+            }
+        }
+    }
+}
