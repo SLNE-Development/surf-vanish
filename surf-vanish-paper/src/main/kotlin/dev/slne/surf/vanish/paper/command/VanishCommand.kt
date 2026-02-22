363679a7558b0fda6c45fcda08902ce8df86cb66
Jo_field <150937665+JoField08@users.noreply.github.com>
2026-02-22 22:23:54 +0100

    feat: add teleport helper command and enhance vanish player handling

--- surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/command/VanishCommand.kt
+++ surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/command/VanishCommand.kt
@@ -6,6 +6,7 @@ import dev.jorel.commandapi.kotlindsl.literalArgument
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 import dev.slne.surf.surfapi.core.api.font.toSmallCaps
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
+import dev.slne.surf.vanish.paper.command.subcommands.teleportHelperCommand
 import dev.slne.surf.vanish.paper.plugin
 import dev.slne.surf.vanish.paper.util.VanishPermissionRegistry
 import dev.slne.surf.vanish.paper.util.displayKey
@@ -14,6 +15,7 @@ import kotlin.system.measureTimeMillis
 
 fun vanishCommand() = commandTree("vanish") {
     withPermission(VanishPermissionRegistry.VANISH_COMMAND)
+    teleportHelperCommand()
     playerExecutor { player, _ ->
         val vanishPlayer = player.vanishPlayer
 
--- surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/util/VanishPermissionRegistry.kt
+++ surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/util/VanishPermissionRegistry.kt
@@ -6,6 +6,7 @@ object VanishPermissionRegistry : PermissionRegistry() {
     const val BASE = "surf.vanish"
     val VANISH_BYPASS = create("$BASE.bypass")
     val VANISH_COMMAND = create("$BASE.command")
+    val VANISH_NOCK_BACK_TP = create("$BASE.no-back-teleport")
     val VANISH_COMMAND_RELOAD = create("$VANISH_COMMAND.reload")
 
     val VANISH_PRIORITY = create("$BASE.priority")
