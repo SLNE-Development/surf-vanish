c3c37113a802f3c9e003b09ce5d6ff5183d861d8
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-11 17:00:07 +0100

    fix: remove unnecessary check for vanished players in HotkeyListener

--- surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/listener/HotkeyListener.kt
+++ surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/listener/HotkeyListener.kt
@@ -21,10 +21,6 @@ object HotkeyListener : PacketListenerAbstract() {
         val player = event.getPlayer<Player>() ?: return
         val vanishPlayer = player.vanishPlayer
 
-        if (!vanishPlayer.isVanished()) {
-            return
-        }
-
         if (!vanishService.isSpectating(player.uniqueId)) {
             return
         }
