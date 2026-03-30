abf599260b77dec1398ceaa0722849eca1f7df34
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-03-30 10:19:41 +0200

    refactor: streamline join message handling for vanished players

--- surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/service/VanishServiceImpl.kt
+++ surf-vanish-paper/src/main/kotlin/dev/slne/surf/vanish/paper/service/VanishServiceImpl.kt
@@ -214,10 +214,10 @@ class VanishServiceImpl : VanishService, Services.Fallback {
                 buildText {
                     spacer(
                         "${
-                            player.currentTarget?.player?.location?.distanceSquared(
+                            player.currentTarget?.player?.location?.distance(
                                 player.location
                             )?.toInt() ?: "Unbekannt"
-                        } Blöcke²".toSmallCaps()
+                        } Blöcke".toSmallCaps()
                     )
                 }
             }
